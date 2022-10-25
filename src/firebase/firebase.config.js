401bf4bfeb18e5d81f8667a2c352f4c7287d7f64
masumkawser <kawser151722@gmail.com>
2022-10-25 18:48:08 +0600

    basic setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTKfINE_vu1mD87Q1LHYvmE-Cyh6k_gVk",
+  authDomain: "binary-academy-c0834.firebaseapp.com",
+  projectId: "binary-academy-c0834",
+  storageBucket: "binary-academy-c0834.appspot.com",
+  messagingSenderId: "315557656299",
+  appId: "1:315557656299:web:f5b26a926170e9f6def0ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
