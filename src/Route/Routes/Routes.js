04d70c9404d08a979d1af7d679d1e393153cc466
masumkawser <kawser151722@gmail.com>
2022-10-25 19:41:47 +0600

    route layout

--- src/Route/Routes/Routes.js
+++ src/Route/Routes/Routes.js
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../../pages/Blog/Blog/Blog";
+import Courses from "../../pages/Courses/Courses/Courses";
+import Home from "../../pages/Home/Home/Home";
 import Main from "./layout/Main";
 
 export const routes =createBrowserRouter([
@@ -7,6 +10,18 @@ export const routes =createBrowserRouter([
     path:'/',
     element:<Main></Main>,
     children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'/courses/:id',
+            element:<Courses></Courses>
+        },
+        {
+            path:'/blog',
+            element:<Blog></Blog>
+        }
 
     ]
 }
--- src/Route/Routes/layout/Main.js
+++ src/Route/Routes/layout/Main.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../../pages/shared/Footer/Footer';
+import Header from '../../../pages/shared/Header/Header';
 
 const Main = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <Header></Header>
+            <Container>
+            <Row>
+                <Col>
+                <h2>side nav</h2></Col>
+                <Col> <Outlet></Outlet></Col>
+            </Row>
+            
+        </Container>
+        <Footer></Footer>
         </div>
     );
 };
