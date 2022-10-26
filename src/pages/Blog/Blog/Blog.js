f16b9dcc7463bc24885f945839d26cffa02c4ffa
masumkawser <kawser151722@gmail.com>
2022-10-26 08:22:55 +0600

    blog q2 added

--- src/pages/Blog/Blog/Blog.js
+++ src/pages/Blog/Blog/Blog.js
@@ -14,15 +14,9 @@ const Blog = () => {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>2. Why are you using firebase? What other options do you have to implement authentication?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app.<br></br> It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
