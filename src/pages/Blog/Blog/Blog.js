f904e76501ed555e49e289e6bcdeb0f8bcfb7951
masumkawser <kawser151722@gmail.com>
2022-10-26 08:25:47 +0600

    blog q3 added

--- src/pages/Blog/Blog/Blog.js
+++ src/pages/Blog/Blog/Blog.js
@@ -20,15 +20,9 @@ const Blog = () => {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #3</Accordion.Header>
+        <Accordion.Header>3. How does the private route work?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).g
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
